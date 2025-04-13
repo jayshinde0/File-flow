0ce8057158baeae3de1d2e60cee3be41202734ae
Jay Shinde <118679090+jayshinde0@users.noreply.github.com>
2025-04-13 12:45:28 +0530

    Add files via upload

---
+++ file-transfer-system/src/main/java/com/example/file_transfer_system/FileTransferSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.file_transfer_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileTransferSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileTransferSystemApplication.class, args);
+	}
+
+}
---
+++ file-transfer-system/src/test/java/com/example/file_transfer_system/FileTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.file_transfer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
